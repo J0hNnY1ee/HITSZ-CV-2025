7415471a608f822b61d303e47f2ef9818be84e21
J0hNnY1ee <joh1eenny@gmail.com>
2025-05-06 21:32:00 +0800

    ✅ 完成作业 4

---
+++ Homework4/network.py
@@ -0,0 +1,39 @@
+'''
+Author: J0hNnY1ee joh1eenny@gmail.com
+Date: 2025-05-06 16:27:24
+LastEditors: J0hNnY1ee joh1eenny@gmail.com
+LastEditTime: 2025-05-06 19:48:45
+FilePath: /HITSZ-CV/Homework4/network.py
+Description: 
+
+Copyright (c) 2025 by J0hNnY1ee joh1eenny@gmail.com, All Rights Reserved. 
+'''
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=6, kernel_size=5, stride=1, padding=0)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1, padding=0)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x) 
+        x = self.pool1(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = self.pool2(x)
+        x = x.view(-1, 16 * 5 * 5)
+        x = self.fc1(x)
+        x = F.relu(x) 
+        x = self.fc2(x)
+        x = F.relu(x) 
+        x = self.fc3(x)
+        return x
